784b9a3eece8241e2d5dad8d1cb9a982d0f3100a
vuvko <notvuvko@gmail.com>
2020-03-29 12:28:26 +0300

    skeleton for io

---
+++ src/io.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import numpy as np
+
+from .mytypes import Task, Result
+from typing import NoReturn
+
+
+def read_task(task_path: Path) -> Task:
+    pass
+
+
+def write_result(result: Result) -> NoReturn:
+    pass
---
+++ src/mytypes.py
@@ -0,0 +1,6 @@
+import numpy as np
+import typing as t
+
+
+Task = t.Any  # TODO:
+Result = t.Any  # TODO:

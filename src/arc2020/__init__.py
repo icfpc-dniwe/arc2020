9fb3a1c33455567603488418b70ba345e9fc3126
vuvko <notvuvko@gmail.com>
2020-04-02 01:05:16 +0300

    solver options

--- src/arc2020/__init__.py
+++ src/arc2020/__init__.py
@@ -4,5 +4,7 @@ from . import io, solve, solver
 
 def main():
     all_tasks = io.read_all_tasks(Path('/kaggle/input/abstraction-and-reasoning-challenge/test'))
-    results = solve.solve(all_tasks, solver.SolverType.GREEDY)
-    io.write_submission((results, results, results), 'submission.csv')
+    results2 = solve.solve(all_tasks, solver.SolverType.GREEDY, max_depth=2)
+    results3 = solve.solve(all_tasks, solver.SolverType.GREEDY, max_depth=3)
+    results4 = solve.solve(all_tasks, solver.SolverType.GREEDY, max_depth=4)
+    io.write_submission((results2, results3, results4), 'submission.csv')
--- src/arc2020/solve.py
+++ src/arc2020/solve.py
@@ -6,9 +6,9 @@ from .mytypes import Result
 from typing import Dict, List
 
 
-def solve(all_tasks: Dict[str, Task], solver_type: SolverType) -> Dict[str, List[Result]]:
+def solve(all_tasks: Dict[str, Task], solver_type: SolverType, **solver_kwargs) -> Dict[str, List[Result]]:
     solver_fn = getattr(solver, str(solver_type)).solve
     results = dict()
     for task_name, cur_task in all_tasks.items():
-        results[task_name] = solver_fn(cur_task)
+        results[task_name] = solver_fn(cur_task, **solver_kwargs)
     return results
